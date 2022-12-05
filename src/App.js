4b1b2cdb79c57a9b8c4322a92a3abdd620538293
Lucy okoth <lucyakokoth@gmail.com>
2022-12-06 01:47:37 +0300

    BF-6 Deleted unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import {BrowserRouter as Router,Routes} from './BrowserRouter';
 
 function App() {
+  const title = 'Welcome';
+  const likes = 50;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+      </div>
     </div>
   );
 }
