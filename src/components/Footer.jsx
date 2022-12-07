104b92ab69fa29a5880d965d5f5994f15df6acf5
Lucy okoth <lucyakokoth@gmail.com>
2022-12-07 11:46:14 +0300

    Deleted unnecessary files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,29 @@
 
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styled from "styled-componets"
-import {
-  faTwitter,
-  faLinkedin,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-import Logo from "./header/Logo";
+// import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import styled from "styled-componets"
+// import {
+//   faTwitter,
+//   faLinkedin,
+//   faInstagram,
+// } from "@fortawesome/free-brands-svg-icons";
+// // import Logo from "./header/Logo";
 
-function Footer() {
-  const date = new Date().getFullYear();
-  return (
-    <StyledFooter>
-      <div className="footer">
-        <div className="socials">
-          <FontAwesomeIcon className="icon" icon={faLinkedin} size="xl" />
-          <FontAwesomeIcon className="icon" icon={faInstagram} size="xl" />
-          <FontAwesomeIcon className="icon" icon={faTwitter} size="xl" />
-        </div>
-        <div className="copy"> Copyright &copy; 2022 </div> <Logo />
-      </div>
-      <div className="copym">Copyright &copy; {date}</div>
-    </StyledFooter>
-  );
-}
+// function Footer() {
+//   const date = new Date().getFullYear();
+//   return (
+//     <styled>
+//       <div className="footer">
+//         <div className="socials">
+//           <FontAwesomeIcon className="icon" icon={faLinkedin} size="xl" />
+//           <FontAwesomeIcon className="icon" icon={faInstagram} size="xl" />
+//           <FontAwesomeIcon className="icon" icon={faTwitter} size="xl" />
+//         </div>
+//         <div className="copy"> Copyright &copy; 2022 </div> <Logo />
+//       </div>
+//       <div className="copym">Copyright &copy; {date}</div>
+//     </styled>
+//   );
+// }
+
+// export default Footer;
