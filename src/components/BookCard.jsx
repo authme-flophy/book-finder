2cf98fd20ddf96a5446432cd27b7a3ab2c09da81
Lucy okoth <lucyakokoth@gmail.com>
2022-12-08 11:05:20 +0300

    Added styling in book.css

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -5,9 +5,11 @@ function BookCard({title, desc, img}) {
     
   return ( 
       <div className="card">
-         <img src={img} class="card-img-top" alt="..."/>
+         
+         <img  src={img} class="card-img-top" alt="..."/>
           <h1>{title}</h1>
           <p>{desc}</p>
+          
       </div>
       // <img className="img" src="" alt=""/>
    );
