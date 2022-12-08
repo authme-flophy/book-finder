975426250cec208c0b334912c04632954144b16a
waikau maina <ericmaina.dev@gmail.com>
2022-12-08 21:48:12 +0300

    made some changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,45 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.css'
 
-function Login() {
+function Login({user, setUser}) {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState([])
+    const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
       e.preventDefault()
       const data = {
         username: username,
         password: password
       }
-      console.log(data)
+      const response = await fetch('/login', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+      });
+      
+      const returnedData = await response.json()
+
+      if (response.ok) {
+        setUser(returnedData)
+        navigate("/")
+      } else (
+        setErrors(returnedData.errors)
+      )
+
     }
 
     return ( 
         <div className="login">
             <form id="login_form">
-              <input type="text" name="username" id="username" placeholder='USERNAME' onChange={e => setUsername(e.target.value)} />
-              <input type="password" name="password" id="password" placeholder='PASSWORD' onChange={e => setPassword(e.target.value)} />
+              <div className='error_message'>{errors[0]}</div>
+              <input type="text" name="username" id="username" placeholder='username' onChange={e => setUsername(e.target.value)} />
+              <input type="password" name="password" id="password" placeholder='password' onChange={e => setPassword(e.target.value)} />
               <button id='submit_button' type="submit" onClick={e => handleSubmit(e)}>LOGIN</button>
             </form>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,42 @@ import { Link } from "react-router-dom";
 import "./Navbar.css"
 
 
-const Navbar = () => {
+const Navbar = ( {user, setUser} ) => {
+
+    const handleLogout = async (e) => {
+
+      console.log('worked');
+
+      const response = await fetch('/logout', {
+        method: 'DELETE',
+      })
+
+      if (response.ok) {
+        setUser(null)
+      }
+
+    }
+
     return ( 
         <nav className="navbar">
-            <div className="links">
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="/books">Book</Link>
-            <Link className="link" to="/login">Login</Link>
-            <Link className="link" to="/signup">Sign Up</Link>
-           
+            
+            {
+              user ? (
+                <div className="links">
+                <Link className="link" to="/">Home</Link>
+                <Link className="link" to="/books">Book</Link>
+                <a className="link" onClick={(e) => handleLogout(e)}>Logout</a>
             </div>
+              ) : (
+                <div className="links">
+                <Link className="link" to="/">Home</Link>
+                <Link className="link" to="/books">Book</Link>
+                  <Link className="link" to="/login">Login</Link>
+                  <Link className="link" to="/signup">Sign Up</Link>
+                </div>
+              )
+            }
+           
         </nav>
      );
 }
